89fe711ab32c174cc6a0ba757ea01ce332599e1b
AmirShayanMobaraki <amirshayan1999@gmail.com>
2024-12-20 02:00:34 +0330

    one line text

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import Login from "./assets/Components/LogIn comp/Login";
 
--- src/assets/Components/LogIn comp/Login.jsx
+++ src/assets/Components/LogIn comp/Login.jsx
@@ -6,9 +6,15 @@ export default function Login() {
   return (
     <>
       <img src={LogoImage} alt="Logo" className="w-16 h-16 ml-8 mt-16" />
-      <h1 className="font-bricolage text-2xl font-bold">
+      <h1 className="font-bricolage text-4xl w-60 font-bold">
         Login to your account
       </h1>
+      <h5 className="font-bricolage font-normal text-gray-700 text-base">
+        Don’t have an account?{" "}
+        <span className="font-bricolage font-semibold text-emerald-500 text-base">
+          Sign Up
+        </span>
+      </h5>
     </>
   );
 }
