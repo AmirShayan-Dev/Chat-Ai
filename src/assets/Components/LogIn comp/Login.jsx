bc3801fa608d4ad2266969f0e2970f88aad2d728
AmirShayanMobaraki <amirshayan1999@gmail.com>
2024-12-20 02:28:11 +0330

    half of Login page is done!

--- src/assets/Components/LogIn comp/Login.jsx
+++ src/assets/Components/LogIn comp/Login.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import LogoImage from "../../Images/LogoChatAi.png";
 import "../../../App.css";
+import EmailIcon from "../../Images/Email-Icon.png";
+import PassIcon from "../../Images/First.png";
+import HideShow from "../../Images/HideShow.png";
 
 export default function Login() {
   return (
@@ -15,6 +18,26 @@ export default function Login() {
           Sign Up
         </span>
       </h5>
+
+      {/* Email Input */}
+      <div className="flex items-center w-[364px] h-[56px] border border-[#EBEDEC] rounded-2xl px-4 bg-transparent">
+        <img src={EmailIcon} alt="Email Icon" className="w-6 h-6 mr-3" />
+        <input
+          type="email"
+          placeholder="Email"
+          className="flex-1 outline-none bg-transparent"
+        />
+      </div>
+      {/* Password Input */}
+      <div className="flex items-center w-[364px] h-[56px] border border-[#EBEDEC] rounded-2xl px-4 bg-transparent">
+        <img src={PassIcon} alt="Pass Icon" className="w-6 h-6 mr-3" />
+        <input
+          type="email"
+          placeholder="Email"
+          className="flex-1 outline-none bg-transparent"
+        />
+        <img src={HideShow} alt="Hide or show" className="w-6 h-6 mr-3" />
+      </div>
     </>
   );
 }
